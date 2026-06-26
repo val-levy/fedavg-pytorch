c0a42118f2e4ae12db6b61d7df2b369dd46ed7a4
Val Levy <146398552+val-levy@users.noreply.github.com>
2026-06-25 20:06:21 -0700

    object impl

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import torch
 from torch import nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 
 from models import TwoNN, CNN
 from server import Server
 from client import Client
+from util import get_dataset
 
 #GLOBALS
 DEVICE = (
@@ -13,3 +14,55 @@ DEVICE = (
     else "cpu"
 )
 
+NUM_CLIENTS = 100
+TOTAL_IMAGES = 60_000
+NUM_ROUNDS = 10
+#set batch size
+TRAIN_BATCHSIZE = 64
+TEST_BATCHSIZE = 1_000
+#set datasets
+train_dataset = get_dataset(train=True)
+test_dataset = get_dataset(train=False)
+
+
+
+def main():
+
+    sizes = []
+    for _ in range(NUM_CLIENTS):
+        sizes.append( TOTAL_IMAGES // NUM_CLIENTS )
+    
+    shards = random_split(train_dataset, sizes)
+
+    clients = []
+    for i, shard in enumerate(shards):
+        client = Client(
+            i, 
+            DataLoader(
+                shard,
+                batch_size=TRAIN_BATCHSIZE, 
+                shuffle=True
+            ), 
+            device=DEVICE
+        )
+        clients.append(client)
+    
+    server = Server(
+        TwoNN(), 
+        clients=clients, 
+        device=DEVICE    
+    )
+
+    #training rounds
+    print("Training...\n\n")
+    for i in range(NUM_ROUNDS):
+        print(f'Training Round: {i}\n')
+        server.train_round()
+        print(f'Round complete.\n')
+
+
+    return
+
+
+if __name__ == "__main__":
+    main()
--- util.py
+++ util.py
@@ -1,1 +1,29 @@
 #pull data
+
+from torch.utils.data import random_split
+
+from torch import DataLoader
+from torchvision import datasets, transforms
+
+transform = transforms.Compose([
+    transforms.ToTensor(), 
+    transforms.Normalize((0.1307,), (0.3081,))
+])
+
+def get_dataset(path='./data', train=False):
+    """
+    Default is test data. Set optional `train` parameter to True to set to train.
+    
+    Default path is './data'.
+    """
+    return datasets.MNIST(
+        root=path,
+        train=train,
+        download=True,
+        transform=transform
+        )
+
+
+# Uncomment out the following lines and run this script to download datasets
+# get_dataset()
+# get_dataset(train=True)
